6afe6953843d9ebc02227c4db80300ded2679ff1
cloudy <272685110@qq.com>
2018-02-27 11:48:06 +0800

    config bus

--- micro-apps/micro-upms/src/main/java/com/hechihan/micro/apps/upms/rest/CompanyRest.java
+++ micro-apps/micro-upms/src/main/java/com/hechihan/micro/apps/upms/rest/CompanyRest.java
@@ -10,8 +10,13 @@ package com.hechihan.micro.apps.upms.rest;
 import com.hechihan.micro.apps.upms.biz.CompanyBiz;
 import com.hechihan.micro.apps.upms.entity.Company;
 import com.hechihan.micro.common.rest.BaseController;
+import com.hechihan.micro.common.vo.ObjectRestResponse;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("company")
 @Api(value = "/company", description = "公司相关接口")
 @Slf4j
+@RefreshScope
 public class CompanyRest extends BaseController<CompanyBiz, Company> {
-
 
 }
